7fd82742d716562eb6738f869e742a3cc6c049d2
Bonface Mudiri <mudirib@gmail.com>
2017-07-20 22:12:04 +0300

    Install normalize.css and create Wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 
+import Wrapper from './Wrapper';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-      
-
+        <Wrapper />
       </div>
     );
   }
---
+++ src/components/Wrapper.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../css/Wrapper.css';
+
+class Wrapper extends Component{
+  render(){
+    return(
+      <div className="Wrapper col-md-3">
+
+      </div>
+    )
+  }
+}
+
+export default Wrapper;
