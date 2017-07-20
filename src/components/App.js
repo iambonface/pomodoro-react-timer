16241b80e26d9985521f974bd768393e5ca26f5e
Bonface Mudiri <mudirib@gmail.com>
2017-07-20 21:21:29 +0300

    Reschuffle components and install bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
+      
+
       </div>
     );
   }
