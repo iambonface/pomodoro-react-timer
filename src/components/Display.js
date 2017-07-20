7862ad4e763482711d5e561064d618c1b88c7349
Bonface Mudiri <mudirib@gmail.com>
2017-07-20 23:20:52 +0300

    Create Display and update Wrap, Wrapper

---
+++ src/components/Display.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../css/Display.css';
+
+class Display extends Component{
+  render(){
+    return(
+      <div className="Display">
+
+      </div>
+    )
+  }
+}
+
+export default Display;
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import '../css/Wrapper.css';
+import Display from './Display';
 
 class Wrapper extends Component{
   render(){
     return(
       <div className="Wrapper col-md-3 col-sm-5 col-xs-10">
-
+        <Display />
       </div>
     )
   }
