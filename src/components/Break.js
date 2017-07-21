4f54f95c63356dc174ffc938e189773f77c12dc0
Bonface Mudiri <mudirib@gmail.com>
2017-07-21 19:47:35 +0300

    Update style in Session, Break

--- src/components/Break.js
+++ src/components/Break.js
@@ -7,7 +7,11 @@ class Break extends Component{
     return(
       <div className="Break col-md-6 col-sm-6 col-xs-6">
       <h4 className="text-center">Break</h4>
-
+        <div className=" count text-center">
+          <button className="btn-min">-</button>
+          <button className="btn-min min">5</button>
+          <button className="btn-min">+</button>
+        </div>
 
       </div>
     )
--- src/components/Session.js
+++ src/components/Session.js
@@ -7,6 +7,11 @@ class Session extends Component{
     return(
       <div className="Session col-md-6 col-sm-6 col-xs-6">
         <h4 className="text-center">Session</h4>
+          <div className=" count text-center">
+            <button className="btn-min">-</button>
+            <button className="btn-min min">25</button>
+            <button className="btn-min">+</button>
+          </div>
 
       </div>
     )
