d401130e5397b186d170511801fbb62bbb074186
Bonface Mudiri <mudirib@gmail.com>
2017-07-30 09:04:28 +0300

    Fix col-xs span and update social btn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,10 @@ class Footer extends Component{
   render(){
     return(
       <div className="Footer col-md-12 col-sm-12 col-xs-12 text-center">
-        <p>Built by Bonface Mudiri &copy;2017 |
-        Fork <a href="https://github.com/iambonface/pomodoro-react-timer" target ="_blank"
-rel="nofollow noopener noreferrer">@Github</a> |
-        Follow <a href="https://www.twitter.com/iambonface" target ="_blank"
-rel="nofollow noopener noreferrer">@iamBonface</a> </p>
+        <p>Built by Bonface Mudiri &copy;2017 <span className="col-md-12 col-sm-12  col-xs-12">
+         Fork<i className="fa fa-github"></i> <a href="https://github.com/iambonface/pomodoro-react-timer" target ="_blank"
+rel="nofollow noopener noreferrer">@iamBonface</a> || Follow<i className="fa fa-twitter"></i> <a href="https://www.twitter.com/iambonface" target ="_blank"
+rel="nofollow noopener noreferrer">@iamBonface</a></span></p>
       </div>
     )
   }
