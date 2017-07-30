ecd0e2e04e7a4fbf03dc3af96cd91a1cddd97d96
Bonface Mudiri <mudirib@gmail.com>
2017-07-30 10:02:36 +0300

    Update Footer social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,9 @@ class Footer extends Component{
       <div className="Footer col-md-12 col-sm-12 col-xs-12 text-center">
         <p>Built by Bonface Mudiri &copy;2017 <span className="col-md-12 col-sm-12  col-xs-12">
          <i className="fa fa-github"></i> <a href="https://github.com/iambonface/pomodoro-react-timer" target ="_blank"
-rel="nofollow noopener noreferrer">Fork</a> | <i className="fa fa-twitter"></i> <a href="https://www.twitter.com/iambonface" target ="_blank"
-rel="nofollow noopener noreferrer">Follow</a></span></p>
+rel="nofollow noopener noreferrer">Fork-Code</a> | <i className="fa fa-twitter"></i> <a href="https://www.twitter.com/iambonface" target ="_blank"
+rel="nofollow noopener noreferrer">Follow-Me</a> | <i className="fa fa-linkedin"></i> <a href="https://www.linkedin.com/in/bonface/" target ="_blank"
+rel="nofollow noopener noreferrer">Hire-Me</a></span></p>
       </div>
     )
   }
