25441c84325c717d458d2cd7db930fa4b7b11da6
Bonface Mudiri <mudirib@gmail.com>
2017-09-04 20:04:06 +0300

    Update ReactGA

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-105774952-1')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
